d883c7f20cf807b98943d3bb7d71d5fab9bfb600
Igor <example@gmail.com>
2021-05-09 22:15:16 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 7;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: "Igor",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj["name"]);
+
+// let arr = ["plum.png", "orange.jpg", 'apple,bmp'];
+
+// console.log(arr[2]);
+
+// alert('Hello');
+
+
+// const result = confirm("Are you hear?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше имя?", '');
+// answers[1] = prompt("Ваша фамилия?", '');
+// answers[2] = prompt("Сколько Вам лет?", '');
+
+// document.write(answers);
+
+// console.log(typeof(answers))
+
+const category = 'toys';
+console.log('https://someurl.com/'+category); 
+console.log(`https://someurl.com/${category}/5`); 
+
+const user = "Ivan";
+
+// alert(`Приывет, ${user}`);
+
+console.log(4+"5");
